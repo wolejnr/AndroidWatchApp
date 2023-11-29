b09e4f3789e90a2b53b7ccb81d668e4fd0d4ad25
aadewumi <wolejr@gmail.com>
2023-11-29 15:46:25 -0500

    Phone to watch app

---
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.testapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.testapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+}
